546866c28979e7ff8725b6b4883c28ae932a45ad
RConradGe <rcgadduang@gmail.com>
2025-03-30 14:53:37 -0700

    wip: added mailbox form

---
+++ src/MailboxForm/MailboxForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+const initialState = {
+  _id: '',
+  boxOwner: '',
+  boxSize: '',
+
+};
+
+const MailboxForm = (props) => {
+  const [formData, setFormData] = useState(initialState);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.addBox(formData);
+    setFormData(initialState);
+    navigate('/mailboxes')
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return(
+    <>
+    <h1>New Mailbox</h1>
+    <form onSubmit={handleSubmit}> 
+      <label htmlFor='boxOwner'>Enter a Boxowner:</label>
+      <input
+        type='text'
+        name='boxOwner'
+        placeholder='Boxholder name'
+        value={formData.boxOwner}
+        onChange={handleChange}
+        required ={true}
+        />
+
+      <label htmlFor='boxSize'>Select a Box Size:</label>
+      <select 
+        type='text'
+        name='boxSize'
+        value= {formData.boxSize}
+        onChange={handleChange
+        }>
+        <option value='small'>Small</option>
+        <option value='medium'>Medium</option>
+        <option value='large'>Large</option>
+      </select>
+      <button type='submit'>Submit</button>
+    </form>
+    </>
+  );
+};
+
+export default MailboxForm;
