2dbd9e4b16d033dae470ff6aaf2326233d7ac494
RConradGe <rcgadduang@gmail.com>
2025-03-30 23:30:59 -0700

    wip: added MailboxNofFound component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './NavBar/NavBar';
 import MailboxList from './MailboxList/MailboxList';
 import MailboxForm from './MailboxForm/MailboxForm';
 import MailboxDetails from './MailboxDetails/MailboxDetails'; 
+import MailboxNotFound from './MailboxNotFound/MailboxNotFound'
 
 import './App.css'
 
@@ -18,6 +19,7 @@ const App = () => {
     setMailboxes([... mailboxes, newMailboxData]);
   };
 
+
   return (
     <>
       <NavBar />
@@ -25,8 +27,8 @@ const App = () => {
         <Route path='/' element={ <main><h1>Post Office</h1></main> } />
         <Route path='/mailboxes' element={<MailboxList mailboxes={mailboxes}/>} />
         <Route path='/new-mailboxes' element={<MailboxForm addBox={addBox} />} />
-        <Route path='mailboxes/:mailboxId' element={<MailboxDetails mailboxes={mailboxes} />} />
-        <Route path='/mailbox-not-found' element={ <h1>Mailbox Not Found!</h1>} />
+        <Route path='mailboxes/:mailboxId' element={<MailboxDetails mailboxes={mailboxes} setMailboxes={setMailboxes}/>} />
+        <Route path='/mailbox-not-found' element={ <MailboxNotFound />} />
         <Route path='*' element={<h1>Whoa, that doesn't exist!</h1>} />
       </Routes>
     </>
