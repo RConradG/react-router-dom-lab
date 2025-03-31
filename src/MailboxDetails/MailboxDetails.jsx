89b73bbb48be59b8b75f1459db110e35cd016580
RConradGe <rcgadduang@gmail.com>
2025-03-30 23:32:12 -0700

    wip: added naviagte to mailbox-not-found url, removed className

--- src/MailboxDetails/MailboxDetails.jsx
+++ src/MailboxDetails/MailboxDetails.jsx
@@ -2,28 +2,29 @@ import {Link} from 'react-router';
 import { useParams, useNavigate } from 'react-router';
 import {useEffect, useState} from 'react';
 
-const MailboxDetails = ({ mailboxes }) => {
+const MailboxDetails =  ({ mailboxes}) => {
   const [singleMailbox, setSingleMailbox] = useState({}); 
   const { mailboxId } = useParams();
   const navigate = useNavigate();
-  
-  useEffect(() => {
-    const oneMailbox = mailboxes.find(oneMailbox => 
-      oneMailbox._id === parseInt(mailboxId)
-    );
+    
+    useEffect( () => {
+      const oneMailbox =  mailboxes.find(oneMailbox => 
+        oneMailbox._id === parseInt(mailboxId)
+      );
 
-    if(!oneMailbox) {
-     return navigate('/mailbox-not-found')
-    };
+      if(!oneMailbox) {
+        navigate('/mailbox-not-found')
+        return
+      };
 
-    setSingleMailbox(oneMailbox);
-    }, [mailboxId, mailboxes, navigate]); 
-
+      setSingleMailbox(oneMailbox);
+    }, [mailboxId, mailboxes, navigate ])
+    
   return (
     <>
     <h1>Mailbox {mailboxId}</h1>
     <h2>Details</h2>
-    <dl className='mail-box'>
+    <dl>
       <dt>Box Number: {singleMailbox._id}</dt>
       <dt>Boxholder: {singleMailbox.boxOwner}</dt>
       <dt>Box Size: {singleMailbox.boxSize}</dt>
